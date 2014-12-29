3082dee6b8a71d30416d7e4e7811c2811c307bfa
Josh <joshua.a.beemster@gmail.com>
2014-12-29 17:33:58 +0100

    Added JsonPath Generator

--- src/main/scala/com.snowplowanalytics/igluutils/generators/JsonPathGenerator.scala
+++ src/main/scala/com.snowplowanalytics/igluutils/generators/JsonPathGenerator.scala
@@ -27,6 +27,101 @@ import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 import org.json4s.scalaz.JsonScalaz._
 
+// Scala
+import scala.collection.immutable.ListMap
+
+/**
+ * Converts lists of keys into a JsonPath file.
+ */
 object JsonPathGenerator {
 
+  private object JsonPathAddOn {
+    val Schema    = "$.schema."
+    val Hierarchy = "$.hierarchy."
+    val Data      = "$.data."
+  }
+
+  private val JsonPathSchemaFields = List(
+    "vendor",
+    "name",
+    "format",
+    "version"
+  )
+
+  private val JsonPathHierarchyFields = List (
+    "rootId",
+    "rootTstamp",
+    "refRoot",
+    "refTree",
+    "refParent"
+  )
+
+  private val JsonPathDefaultWrapper = parse("""{"jsonpaths":[]}""")
+
+  /**
+   * Returns a valid JsonPath file based on the default
+   * fields provided and the flattened schema.
+   *
+   * @param flatSchema A flattened schema containing all
+   *        the paths that need to be added for the data
+   *        fields
+   * @return a JsonPath containing all of the relevant
+   *         fields
+   */
+  def getJsonPath(flatSchema: Map[String, ListMap[String, Map[String,String]]]): JValue = {
+
+    // Convert all of the field lists to lists of JStrings
+    val schemaFieldList = transformStringList(JsonPathSchemaFields, JsonPathAddOn.Schema)
+    val hierarchyFieldList = transformStringList(JsonPathHierarchyFields, JsonPathAddOn.Hierarchy)
+    val dataFieldList = flatSchema.get("flat_elems") match {
+      case Some(elems) => transformStringList(elems.keys.toList, JsonPathAddOn.Data)
+      case None => List()
+    }
+
+    // Combine all of the lists into one
+    val fieldList = schemaFieldList ++ hierarchyFieldList ++ dataFieldList
+
+    // Insert the field list into the JsonPath default wrapper and return
+    JsonPathDefaultWrapper transformField {
+      case ("jsonpaths", JArray(_)) => ("jsonpaths", JArray(fieldList))
+    }
+  }
+
+  /**
+   * Transforms a list of strings into a list of JStrings
+   * which can be nested into a JArray JValue.
+   *
+   * @param strList The list of strings to be converted to
+   *        JStrings
+   * @param strAdd The string to be appended to the start
+   *        of each string
+   * @return the List of converted Strings
+   */
+  private def transformStringList(strList: List[String], strAdd: String): List[JValue] =
+    for {
+      string <- strList
+    } yield {
+      JString(strAdd + string)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
